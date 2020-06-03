7a2c08217b4fa303ed287941fcad3baa37dcc6f9
Jay <jordanchu95@gmail.com>
2020-06-03 13:11:00 -0400

    swipe right logic

--- src/App.js
+++ src/App.js
@@ -91,6 +91,51 @@ function App() {
     }
   };
 
+  const swipeRight = (dummy) => {
+    let oldData = data;
+    let newArray = cloneDeep(data);
+
+    for (let i = 3; i >= 0; i--) {
+      let b = newArray[i];
+      let slow = b.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast--;
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast];
+          b[fast] = 0;
+          fast--;
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast--;
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast];
+            b[fast] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(newArray) !== JSON.stringify(oldData)) {
+      addNumber(newArray);
+    }
+    if (dummy) {
+      return newArray;
+    } else {
+      setData(newArray);
+    }
+  };
+
   const handleKeyDown = event =>{
     switch (event.keyCode) {
       case LEFT_ARROW:
