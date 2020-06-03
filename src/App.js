eb890d81ac0adca69099f7fc87ac946d7b4959b3
Jay <jordanchu95@gmail.com>
2020-06-03 12:44:14 -0400

    adding in a random number to the grid

--- src/App.js
+++ src/App.js
@@ -11,7 +11,26 @@ function App() {
   //initialize
 
   //add number, add  an item to the grid
+  const addNumber = (newGrid) => {
+    let added = false;
+    let gridFull = false;
+    let attempts = 0;
 
+    while (!added) {
+      if (gridFull) {
+        break;
+      }
+
+      let random1 = Math.floor(Math.random() * 4);
+      let random2 = Math.floor(Math.random() * 4);
+      attempts ++;
+
+      if (newGrid[random1][random2] === 0) {
+        newGrid[random1][random2] = Math.random() > 0.5 ? 2 : 4;
+        added = true;
+      }
+    }
+  }
   //swipe right, left, up, down
 
   // check game over
