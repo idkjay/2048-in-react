09b60530aeeff69b9ee2cfb4b5b159d2361302b7
Jay <jordanchu95@gmail.com>
2020-06-04 11:14:39 -0400

    swipe  down logic

--- src/App.js
+++ src/App.js
@@ -184,6 +184,50 @@ function App() {
     }
   };
 
+  const swipeDown = (dummy) => {
+    console.log(data);
+    let b = cloneDeep(data);
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 3; i >= 0; i--) {
+      let slow = b.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (b[slow][i] === 0 && b[fast][i] === 0) {
+          fast--;
+        } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+          b[slow][i] = b[fast][i];
+          b[fast][i] = 0;
+          fast--;
+        } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+          fast--;
+        } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+          if (b[slow][i] === b[fast][i]) {
+            b[slow][i] = b[slow][i] + b[fast][i];
+            b[fast][i] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(b) !== JSON.stringify(oldData)) {
+      addNumber(b);
+    }
+    if (dummy) {
+      return b;
+    } else {
+      setData(b);
+    }
+  };
+
   const handleKeyDown = event =>{
     switch (event.keyCode) {
       case LEFT_ARROW:
